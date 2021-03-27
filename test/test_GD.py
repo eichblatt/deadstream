5e7d08e2a8934bb251d02d693494a731094fc5b7
Derek Wisong <derekwisong@gmail.com>
2021-03-27 19:34:11 -0400

    Add sync and async test cases

--- test/test_GD.py
+++ test/test_GD.py
@@ -3,16 +3,21 @@ import tempfile
 
 import GD
 
+
 class TestGD(unittest.TestCase):
 
     def test_create_set(self):
         set = GD.GDSet()
         self.assertGreater(len(set.set_data), 0)
-    
-    def test_create_archive(self):
-        # This unit test is slow because it initializes a new archive
+
+    def test_create_async_archive(self):
         with tempfile.TemporaryDirectory() as directory:
-            archive = GD.GDArchive(directory)
+            GD.GDArchive(directory, sync=False)
+
+    def test_create_sync_archive(self):
+        with tempfile.TemporaryDirectory() as directory:
+            # slow: initializes a new archive synchronously
+            GD.GDArchive(directory, sync=True)
 
 if __name__ == '__main__':
     unittest.main()
