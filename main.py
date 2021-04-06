09e005477450cb6874fde2410dfb2b37f8393bb1
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-04-06 03:10:30 +0100

    pause and restart is now working!

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import config
 from time import sleep
 import logging
 import signal
+import sys
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
@@ -13,6 +14,7 @@ parser = optparse.OptionParser()
 parser.add_option('--debug',dest='debug',default=True)
 parser.add_option('--verbose',dest='verbose',default=False)
 parms,remainder = parser.parse_args()
+
 
 meInterrupt = False
 def meCustomHandler(signum,stack_frame):
@@ -22,15 +24,28 @@ def meCustomHandler(signum,stack_frame):
 
 signal.signal(signal.SIGINT, meCustomHandler)
 
-def meLoop(knobs,a,scr):
+def play_tape(tape):
+    if player != None: return player
+    logging.info(F"Playing tape {tape}")
+    player = GD.GDPlayer(tape)
+    player.play()
+    return player
+
+def pause_tape(player,tape):
+   logging.info(F"Pausing {date_fmt} on player")
+   player.pause()
+   play_state = False
+   scr.show_playstate('paused')
+ 
+def meLoop(knobs,a,scr,player,maxN=None):
     y,m,d = knobs
     play_state = config.PLAY_STATE
     d0 = (ctl.date_knob_reader(y,m,d,a)).date
-    player = None
+    N = 0
 
-    while True:
+    while True and N<=(maxN if maxN != None else True):
       staged_date = ctl.date_knob_reader(y,m,d,a)
-      if staged_date.date != d0: 
+      if staged_date.date != d0:  # Date knobs changed
         logging.info (F"DATE: {config.DATE}, SELECT_DATE: {config.SELECT_DATE}, PLAY_STATE: {config.PLAY_STATE}")
         print (staged_date)
         d0 = staged_date.date
@@ -41,41 +56,47 @@ def meLoop(knobs,a,scr):
     #    else:
     #      scr.draw.rectangle((0,0,160,32),outline=0,fill=(0,0,0)) # erase the venue
     #      scr.disp.image(scr.image)
-      if config.SELECT_DATE:
+      elif config.SELECT_DATE:   # Year Button was Pushed
         if staged_date.tape_available():
            config.DATE = staged_date.date 
            date_fmt = config.DATE.strftime('%Y-%m-%d')
            logging.info(F"Setting DATE to {date_fmt}")
-           tape = a.best_tape(date_fmt)
-           player = GD.GDPlayer(tape)
            scr.show_date(config.DATE,loc=(85,0),size=10,color=(255,255,255),stack=True,tape=True)
         config.SELECT_DATE = False
-      if config.PLAY_STATE and not play_state:  # start playing
+      elif (config.PLAY_STATE == 3) and (play_state < 3):  #  Day Button was Pushed while not playing
          try:
            date_fmt = config.DATE.strftime('%Y-%m-%d')
            logging.info(F"Playing {date_fmt} on player")
-           player.play()
+           tape = a.best_tape(date_fmt)
+           if player == None: player = play_tape(tape)
+           else: player.play()
+           play_state = 3
            scr.show_playstate('playing')
          except AttributeError:
            logging.info(F"Cannot play date {config.DATE}")
-           config.PLAY_STATE = play_state
            pass
          except:
            logging.info(F"Unexpected Error: {sys.exc_info()[0]}")
-           config.PLAY_STATE = play_state
            raise 
-      if not config.PLAY_STATE and play_state:  # pause playing
+         finally:
+           config.PLAY_STATE = play_state
+      elif (config.PLAY_STATE < 3) and (play_state == 3):  # Day Button pushed while playing
          try:
+           date_fmt = config.DATE.strftime('%Y-%m-%d')
            logging.info(F"Pausing {date_fmt} on player")
            player.pause()
+           play_state = 1
            scr.show_playstate('paused')
          except:
            logging.info(F"Unexpected Error: {sys.exc_info()[0]}")
-           config.PLAY_STATE = play_state
            raise 
+         finally:
+           config.PLAY_STATE = play_state
       play_state = config.PLAY_STATE
-      if meInterrupt: return
+      if meInterrupt: return player
+      N = N+1
       sleep(.1)
+    return player
 
 def main(parms):
     y = ctl.knob((16,22,23),"year",range(1965,1996),1979)   # cl, dt, sw
@@ -96,8 +117,9 @@ def main(parms):
     scr.clear()
     scr.show_date(staged_date.date,tape=staged_date.tape_available())
     #scr.show_text(staged_date.venue())
+    player = None
     try:
-      meLoop((y,m,d),a,scr)
+      player = meLoop((y,m,d),a,scr,player,maxN=50)
     finally:
       print("In Finally")
       [x.cleanup() for x in [y,m,d]] ## redundant, only one cleanup is needed!
