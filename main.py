c8da420eae98b4764da4fa2fbe0c05356316780a
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-04-09 02:39:02 +0100

    moving test to main

--- main.py
+++ main.py
@@ -31,10 +31,11 @@ def play_tape(tape,player):
     return player
 
 def runLoop(knobs,a,scr,player,maxN=None):
+    global meInterrupt
     y,m,d = knobs
     play_state = config.PLAY_STATE
     d0 = (ctl.date_knob_reader(y,m,d,a)).date
-    N = 0
+    N = 0; prev_track = None
 
     while N<=maxN if maxN != None else True:
       staged_date = ctl.date_knob_reader(y,m,d,a)
@@ -45,27 +46,40 @@ def runLoop(knobs,a,scr,player,maxN=None):
          logging.info (F"DATE: {config.DATE}, SELECT_DATE: {config.SELECT_DATE}, PLAY_STATE: {config.PLAY_STATE}")
          print (staged_date)
          d0 = staged_date.date
-         scr.show_date(staged_date.date,tape=staged_date.tape_available())
-    #     if staged_date.tape_available(): 
-    #        venue = staged_date.venue()
-    #        scr.show_text(venue)
-    #     else:
-    #        scr.draw.rectangle((0,0,160,32),outline=0,fill=(0,0,0)) # erase the venue
-    #        scr.disp.image(scr.image)
+         if staged_date.tape_available(): 
+            scr.show_venue(staged_date.venue())
+         else:
+            scr.clear_area((0,30,160,30)) # erase the venue
+         scr.show_staged_date(staged_date.date)
       if config.SELECT_DATE:   # Year Button was Pushed
          if staged_date.tape_available():
             config.DATE = staged_date.date 
             logging.info(F"Setting DATE to {config.DATE}")
             config.PLAY_STATE = config.READY  #  eject current tape, insert new one in player
-    #        scr.show_date(config.DATE,loc=(85,0),size=10,color=(255,255,255),stack=True,tape=True)
             scr.show_selected_date(config.DATE)
          config.SELECT_DATE = False
+         scr.show_playstate()
 
       # Deal with PLAY_STATE changes
 
       #PLAY_STATE = config.PLAY_STATE
 
-      if (config.PLAY_STATE == play_state): sleep(0.1); continue
+      current_track = player._get_property('playlist-pos')
+      if (config.PLAY_STATE == play_state): 
+         if (config.PLAY_STATES[config.PLAY_STATE] in ['Playing','Paused']) and current_track != prev_track:
+            prev_track = current_track
+            title = player.tape.tracks()[current_track].title
+            scr.show_track(title,0)
+            if (current_track+1)<len(player.playlist):
+               next_track = current_track+1 if (current_track+1)<len(player.playlist) else None
+               next_title = player.tape.tracks()[next_track].title
+               scr.show_track(next_title,1)
+            else: scr.show_track(" ",1)
+            scr.show_playstate()
+         sleep(0.1); continue
+
+      # now, config.PLAY_STATE != play_state
+
       if (config.PLAY_STATE == config.READY):  #  A new tape to be inserted
          player.eject_tape()
          tape = a.best_tape(config.DATE.strftime('%Y-%m-%d'))
@@ -89,11 +103,12 @@ def runLoop(knobs,a,scr,player,maxN=None):
       if config.PLAY_STATE == config.PAUSED: 
          logging.info(F"Pausing {config.DATE.strftime('%Y-%m-%d')} on player") 
          player.pause()
+         scr.show_playstate()
       if config.PLAY_STATE == config.STOPPED:
          player.stop()
          scr.show_playstate()
       play_state = config.PLAY_STATE
-      scr.show_playstate()
+      #scr.show_playstate()
       sleep(.1)
 
 def main(parms):
@@ -103,21 +118,19 @@ def main(parms):
     m = ctl.knob(config.month_pins,"month",range(1,13),11)
     d = ctl.knob(config.day_pins,"day",range(1,32),2,bouncetime=100)
 
-    scr = ctl.screen()
-    scr.clear()
-    scr.show_text("Grateful\n  Dead\n   Streamer",color=(0,255,255))
     _ = [x.setup() for x in [y,m,d]]
 
     logging.info ("Loading GD Archive")
     a = GD.GDArchive('/home/steve/projects/deadstream/metadata')
     logging.info ("Done ")
-    scr.clear()
 
     staged_date = ctl.date_knob_reader(y,m,d,a)
-    scr.show_date(staged_date.date,tape=staged_date.tape_available())
     print (staged_date)
 
-    #scr.show_text(staged_date.venue())
+    scr = ctl.screen()
+    scr.clear()
+    scr.show_staged_date(staged_date.date)
+    scr.show_venue(staged_date.venue())
     loop = threading.Thread(target=runLoop,name="deadstream loop",args=((y,m,d),a,scr,player),kwargs={'maxN':None})
     loop.start()
 
@@ -127,4 +140,3 @@ def main(parms):
 print(F"parms: {parms}")
 if __name__ == "__main__" and parms.debug==0:
   main(parms)
-
