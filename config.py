ad91422fa177d28356f92e6d9a672edc74e1d434
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-04-09 20:35:53 +0100

    adding options

--- config.py
+++ config.py
@@ -16,3 +16,13 @@ year_pins = (16,22,23)   # cl, dt, sw
 month_pins = (12,5,6)
 day_pins = (13,17,27)
 
+select_pin = 23
+play_pause_pin = 6
+stop_pin = 27
+ffwd_pin = None 
+rew_pin = None
+
+# Options
+
+ON_TOUR = False
+FADE_AWAY = "NOT"  # or "WEST_LA"
