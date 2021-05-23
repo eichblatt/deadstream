fa243f71100605f50360dad722aae8572afe6848
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-05-22 20:52:37 -0400

    not quite saving yet

--- configure.py
+++ configure.py
@@ -9,7 +9,7 @@ import cherrypy
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 parser = optparse.OptionParser()
 parser.add_option('-d','--debug',dest='debug',type="int",default=1,help="If > 0, don't run the main script on loading [default %default]")
-parser.add_option('--options_path',dest='options_path',default=os.path.join(ROOT_DIR,'options.txt'),help="path to options file [default %default]")
+parser.add_option('--options_path',dest='options_path',default=os.path.join(ROOT_DIR,'timemachine','options.txt'),help="path to options file [default %default]")
 parser.add_option('--sleep_time',dest='sleep_time',type="int",default=10,help="how long to sleep before checking network status [default %default]")
 parser.add_option('-v','--verbose',dest='verbose',action="store_true",default=False,help="Print more verbose information [default %default]")
 parms,remainder = parser.parse_args()
@@ -21,19 +21,24 @@ logger = logging.getLogger(__name__)
 class StringGenerator(object):
     @cherrypy.expose
     def index(self):
+        f =open(parms.options_path,'r')
+        opt_dict = json.loads(f.read())
+        form_strings = [F'<label>{x[0]} <input type="text" value="{x[1]}" name="{x[0]}" /></label> <p>' for x in opt_dict.items()]
+        form_string = '\n'.join(form_strings)
         return """<html>
           <head></head>
           <body>
-            <form method="get" action="generate">
-              <input type="text" value="8" name="length" />
-              <button type="submit">Give it now!</button>
+            <form method="get" action="save_values">""" + form_string + """
+              <button type="submit">Submit</button>
             </form>
           </body>
         </html>"""
 
     @cherrypy.expose
-    def generate(self, length=8):
-        return ''.join(random.sample(string.hexdigits, int(length)))
+    def save_values(self,*args,**kwargs):
+       with open(parms.options_path, 'w') as outfile:
+         opt_dict = json.dumps(kwargs,indent=0)
+       print(F'args: {args},kwargs:{kwargs}')
 
 def main(parms):
   cherrypy.config.update({'server.socket_host':'192.168.0.21','server.socket_port':9090})
