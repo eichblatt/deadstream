3330b51508bbb7260740323fb669aff97af15e77
eichblatt <steve.eichblatt@gmail.com>
2021-05-25 18:32:26 -0400

    modified:   services.sh

--- bin/services.sh
+++ bin/services.sh
@@ -3,6 +3,8 @@
 sudo cp timemachine.service /etc/systemd/system/.
 sudo cp connect_network.service /etc/systemd/system/.
 sudo chmod 644 /etc/wpa_supplicant/wpa_supplicant.conf
+sudo chown root /etc/wpa_supplicant/wpa_supplicant.conf
+sudo chgrp root /etc/wpa_supplicant/wpa_supplicant.conf
 
 sudo systemctl daemon-reload
 sudo systemctl enable timemachine.service 
