16342959e3ec5e17a4621d2be82b10dd24336c92
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-06-04 09:34:29 -0400

    adding board_version.sh to dev branch

---
+++ bin/board_version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read lines words chars filename <<<$(cat /boot/config.txt | grep gpio-shutdown | grep -v ^# | wc)
+
+if [ "$lines" -eq "0" ]; then
+   echo "version 1";
+   exit 0
+fi
+echo "version 2"
