48c85d5d61fd1e6e6d4fb73bf9b7a9194ae0a720
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-06-16 12:18:46 -0400

    adding serve_options service

--- bin/services.sh
+++ bin/services.sh
@@ -2,6 +2,7 @@
 
 sudo cp timemachine.service /etc/systemd/system/.
 sudo cp connect_network.service /etc/systemd/system/.
+sudo cp serve_options.service /etc/systemd/system/.
 sudo chmod 644 /etc/wpa_supplicant/wpa_supplicant.conf
 sudo chown root /etc/wpa_supplicant/wpa_supplicant.conf
 sudo chgrp root /etc/wpa_supplicant/wpa_supplicant.conf
@@ -9,3 +10,6 @@ sudo chgrp root /etc/wpa_supplicant/wpa_supplicant.conf
 sudo systemctl daemon-reload
 sudo systemctl enable timemachine.service
 sudo systemctl enable connect_network.service
+sudo systemctl enable serve_options.service
+
+exit 0
