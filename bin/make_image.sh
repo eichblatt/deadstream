648e64ce1890e00e7ea31309d93fe29ea18a8c69
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-06-25 16:42:21 -0400

    modified:   make_image.sh 	remove image file before creating a new one.

--- bin/make_image.sh
+++ bin/make_image.sh
@@ -6,7 +6,7 @@ image_date=`date +%Y%m%d`
 image_file=$SCRIPT_DIR/$version\_$image_date.img
 media_folder=/media/steve
 
-critical_command () { 
+critical_command () {
    command=$1
    echo "$command"
    $command
@@ -17,11 +17,12 @@ critical_command () {
    fi
 }
 
-system () { 
+system () {
    command=$1
    echo "$command"
    $command
 }
+
 
 echo "Removing previous git folders"
 system "sudo rm -rf $media_folder/rootfs/home/deadhead/deadstream_prev*"
@@ -31,10 +32,11 @@ system "sudo rm $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf*"
 
 media_dirs="/dev/sdb1 /dev/sdb2"
 
-for dir in $media_dirs; do	
+for dir in $media_dirs; do
     system "sudo umount $dir"
 done
 
+system "sudo rm $image_file"
 critical_command "sudo dd if=/dev/sdb of=$image_file bs=4M status=progress"
 
 system "sudo pishrink.sh $image_file"
