e28a38662766976d585c59db47b2160cc4055557
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-06-29 09:38:12 -0400

    remove package_data

--- setup.py
+++ setup.py
@@ -29,11 +29,11 @@ setup(
         'tenacity',
         'wheel'
     ],
+    #package_data={
+    #    "timemachine": ["ariallgt.ttf", "DejaVuSansMono-Bold.ttf", "FreeMono.ttf", "set_breaks.csv", "fonts/*ttf"],
     entry_points={'console_scripts':
                   ['timemachine=timemachine.main:main',
                    'timemachine_test_update=timemachine.main:main_test_update'
-                   ]},
-    package_data={
-        "timemachine": ["ariallgt.ttf", "DejaVuSansMono-Bold.ttf", "FreeMono.ttf", "set_breaks.csv", "fonts/*ttf"]
+                   ]}
     }
 )
