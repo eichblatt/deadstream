8b650e7a7f5340c49b5266448db48c4984018c70
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-06-30 12:07:02 -0400

    using package methodology -- much cleaner

--- timemachine/bin/update.sh
+++ timemachine/bin/update.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
 echo "home is $HOME"
-# Setup directories.
-test_dir_name=deadstream_tmp
-git_user=eichblatt
-repo_name=deadstream.git
-project_dir=$HOME/deadstream
-test_dir=$HOME/$test_dir_name
+echo "Updating "
+date
 
-echo "project_dir=$project_dir"
-echo "test_dir=$test_dir"
-echo "home is $HOME"
-echo "user is $USER"
+git_branch=dev    # Make this a command-line option!
 
+system () {
+   command=$1
+   echo "$command"
+   $command
+}
 
 restore_services () {
    # put the old services back in place.
-   echo "cd $project_dir/bin"
-   cd $project_dir/bin
-   echo "./services.sh"
-   ./services.sh
+   echo "services.sh"
+   services.sh
    # Restart the services (Can i get the timemachine service to launch the serve_options?)
    echo "sudo service timemachine restart"
    sudo service timemachine restart
@@ -27,29 +23,50 @@ restore_services () {
    sudo service serve_options restart
 }
 
-cleanup_old_tmp_dirs () {
-   echo "Cleaning up old tmp dirs ... "
-   echo "ls -1trd $HOME/deadstream_prev* | head -n -8"
-   ls -1trd $HOME/deadstream_prev* | head -n -8
-   echo "ls -1trd $HOME/deadstream_prev* | head -n -8 | xargs -d '\n' rm -f --"
-   ls -1trd $HOME/deadstream_prev* | head -n -8 | xargs -d '\n' rm -f --
-   echo "Done cleaning up old tmp dirs "
+cleanup_old_envs () {
+   echo "Cleaning up old envs ... "
+   echo "ls -1trd $HOME/env_* | head -n -8"
+   ls -1trd $HOME/env_* | head -n -8
+   echo "ls -1trd $HOME/env* | head -n -8 | xargs -d '\n' rm -f --"
+   ls -1trd $HOME/env_* | head -n -8 | xargs -d '\n' rm -f --
+   echo "Done cleaning up old envs"
 }
 
-echo "Updating "
-date
+system "cd $HOME"
+env_name=env_`date +%Y%m%d`.`cat /dev/random | tr -cd 'a-f0-9' | head -c 8`
+system "python3 -m venv $env_name"
+system "source $env_name/bin/activate"
+system "pip3 install wheel"
+system "pip3 install git+https://github.com/eichblatt/deadstream.git@$git_branch"
+
+current_metadata_path=$HOME/timemachine/lib/python3.7/site-packages/timemachine/metadata
+new_metadata_path=$HOME/$env_name/lib/python3.7/site-packages/timemachine/metadata
+update_archive=`find $current_metadata_path/GratefulDead_ids.json -mtime +40 | wc -l`
+if [ $update_archive == 0 ]; then
+   echo "cp -pR $current_metadata_path/*.json $new_metadata_path/."
+   cp -pR $current_metadata_path/*.json $new_metadata_path/.
+fi
 
 # Stop the running services
-echo "sudo service timemachine stop"
-sudo service timemachine stop
-echo "sudo service serve_options stop"
-sudo service serve_options stop
+system "sudo service timemachine stop"
+system "sudo service serve_options stop"
 
-cd $project_dir
-git_branch=`git branch | awk '/\*/ {print $2}'`
-echo "git branch: $git_branch"
+system "timemachine_test_update"
+stat=$?
+echo "status of test command: $stat"
 
-git remote update
+system "cd $HOME" # NOTE: we should already be here.
+if [ $stat == 0 ]; then
+   system "rm timemachine && ln -s $env_name timemachine"
+else
+   system "rm -rf $env_name"
+fi
+
+restore_services
+cleanup_old_envs
+
+exit $stat
+
 updated_code=`git status -uno | grep "up to date" | wc -l`
 if [ $updated_code == 1 ]; then
    echo "No new code. Not updating "
@@ -57,75 +74,3 @@ if [ $updated_code == 1 ]; then
    restore_services
    exit 0
 fi
-
-# check if archive needs refreshing
-echo "find $project_dir/timemachine/metadata/GratefulDead_ids.json -mtime +40 | wc -l"
-update_archive=`find $project_dir/timemachine/metadata/GratefulDead_ids.json -mtime +40 | wc -l`
-echo "update_archive = $update_archive"
-
-# clone the repo into the test_dir
-cd $HOME
-rm -rf $test_dir
-mkdir -p $test_dir
-cd $HOME
-echo "git clone https://github.com/$git_user/$repo_name $test_dir_name"
-git clone https://github.com/$git_user/$repo_name $test_dir_name
-echo "cd $test_dir"
-cd $test_dir
-echo "git remote set-url origin git@github.com:$git_user/$repo_name"
-git remote set-url origin git@github.com:$git_user/$repo_name
-echo "git config core.hooksPath .githooks/"
-git config core.hooksPath .githooks/
-
-echo "git checkout $git_branch"
-git checkout $git_branch
-
-## Derek suggests: create a venv, then pip3 install http://github...repo
-pip3 install .
-
-# If the archive has been refreshed in the last 40 days, copy it to the test dir
-if [ $update_archive == 0 ]; then
-   echo "sudo cp -pR $project_dir/timemachine/metadata/*.json $test_dir/timemachine/metadata/."
-   sudo cp -pR $project_dir/timemachine/metadata/*.json $test_dir/timemachine/metadata/.
-fi
-
-# Set up the services. NOTE: Only for versions > 1 (because username was steve, not deadhead)
-# NOTE: If there is something wrong with the service command, and it doesn't start we are screwed.
-# So we are going to need factory reset command
-
-cd $test_dir/bin
-#version_1=`./board_version.sh | grep "^version 1$" | wc -l`
-if [ $user == deadhead ]; then
-    echo "pwd is `pwd`"
-    ./services.sh
-    stat=$?
-    if [ $stat != 0 ]; then
-       echo "status of services command: $stat"
-       echo "rm -rf $test_dir"
-       rm -rf $test_dir
-
-       restore_services
-       # exit with failure.
-       cleanup_old_tmp_dirs
-       exit $stat
-    fi
-fi
-
-# Run the main program, make sure a button press and knob turn work.
-python3 $test_dir/main.py --test_update
-stat=$?
-echo "status of test command: $stat"
-
-backup_dir=$HOME/deadstream_previous.`cat /dev/random | tr -cd 'a-f0-9' | head -c 12`
-# If this succeeds, put the new folder in place.
-if [ $stat == 0 ]; then
-   echo "mv $project_dir $backup_dir"
-   mv $project_dir $backup_dir
-   echo "mv $test_dir $project_dir"
-   mv $test_dir $project_dir
-fi
-
-restore_services
-cleanup_old_tmp_dirs
-
-exit $stat
