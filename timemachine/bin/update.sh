bfc5d05b1c53ac07ad1b3611d6252a3a5b374e5f
Steve Eichblatt <steve.eichblatt@gmail.com>
2021-07-10 13:17:36 -0400

    use pkl files instead of json

--- timemachine/bin/update.sh
+++ timemachine/bin/update.sh
@@ -49,10 +49,11 @@ system "pip3 install git+https://github.com/eichblatt/deadstream.git@$git_branch
 
 current_metadata_path=$HOME/timemachine/lib/python3.7/site-packages/timemachine/metadata
 new_metadata_path=$HOME/$env_name/lib/python3.7/site-packages/timemachine/metadata
-update_archive=`find $current_metadata_path/GratefulDead_ids.json -mtime +40 | wc -l`
+update_archive=`find $current_metadata_path/GratefulDead_ids.pkl -mtime +40 | wc -l`
+#update_archive=`find $current_metadata_path/GratefulDead_ids.json -mtime +40 | wc -l`
 if [ $update_archive == 0 ]; then
-   echo "cp -pR $current_metadata_path/*.json $new_metadata_path/."
-   cp -pR $current_metadata_path/*.json $new_metadata_path/.
+   system "cp -pR $current_metadata_path/*.json $new_metadata_path/."
+   system "cp -pR $current_metadata_path/*.pkl $new_metadata_path/."
 fi
 
 # Stop the running services
