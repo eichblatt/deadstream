ca72eccedf2deb7e42ca2611f2521e14409b2922
eichblatt <steve.eichblatt@gmail.com>
2021-09-23 11:25:15 -0400

    keep copy of .knob_sense

--- timemachine/bin/make_image.sh
+++ timemachine/bin/make_image.sh
@@ -29,13 +29,14 @@ system "cd $media_folder/rootfs/home/deadhead"
 current_env=$(basename `readlink -f timemachine`)
 files=`find . -maxdepth 1 -mindepth 1 -name env_\* -a -not -name $current_env -printf "%f "`
 system "sudo rm -rf $files"
-system "sudo rm -rf .knob_sense"
-system "sudo rm -rf .ssh"
+system "sudo mv .knob_sense $HOME/.knob_sense"
+system "sudo rm -rf .ssh .bash_history .python_history .viminfo .wget-hsts .gnupg .lesshst .ipython .local"
+system "sudo rm -rf .factory_env"
 system "sudo cp -r $current_env .factory_env"
 
 
 echo "Removing wpa_supplicant"
-system "sudo mv $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf $media_folder/rootfs/etc/wpa_supplicant/tmp_wpa_supplicant.conf"
+system "sudo mv $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf $HOME/wpa_supplicant.conf"
 system "sudo rm $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf*"
 
 media_dirs="/dev/sdb1 /dev/sdb2"
@@ -48,8 +49,9 @@ system "sudo rm $image_file"
 critical_command "sudo dd if=/dev/sdb of=$image_file bs=4M status=progress"
 
 system "sudo pishrink.sh $image_file"
-echo "Replacing wpa_supplicant"
-system "sudo mv $media_folder/rootfs/etc/wpa_supplicant/tmp_wpa_supplicant.conf $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf"
+echo "Replacing wpa_supplicant and knob_sense files"
+system "sudo mv $HOME/wpa_supplicant.conf $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf"
+system "mv $HOME/.knob_sense $media_folder/rootfs/home/deadhead/."
 
 # NOTE: to burn an image use the command (or similar):
 # sudo sh -c "pv v2_20210625.img > /dev/sdb"
