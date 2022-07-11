493d95935c9a6628f09648759583765d4d1f7449
eichblatt <steve.eichblatt@gmail.com>
2022-07-11 14:11:15 -0400

    echo every line. Put a wpa_supplicant in place

--- timemachine/bin/make_image.sh
+++ timemachine/bin/make_image.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 version=v2
@@ -51,38 +51,42 @@ system () {
 
 
 echo "Removing previous git folders"
-system "cd $media_folder/rootfs/home/deadhead"
+cd $media_folder/rootfs/home/deadhead
 current_env=$(basename `readlink -f timemachine`)
 files=`find . -maxdepth 1 -mindepth 1 -name env_\* -a -not -name $current_env -printf "%f "`
-system "sudo rm -rf $files"
-system "sudo mv .knob_sense $HOME/.knob_sense"
-system "sudo mv .timemachine_options.txt $HOME/.timemachine_options.txt"
-system "sudo mv .ssh $HOME/timemachine_dot_ssh"
-system "sudo rm -rf .ssh .bash_history .python_history .viminfo .wget-hsts .gnupg .lesshst .ipython .local .gitconfig .xsession*"
-system "sudo rm -rf .factory_env"
-system "sudo cp -r $current_env .factory_env"
-system "sudo cp $HOME/test_sound.ogg ."
+sudo rm -rf $files
+sudo mv .knob_sense $HOME/.knob_sense
+sudo mv .timemachine_options.txt $HOME/.timemachine_options.txt
+sudo mv .ssh $HOME/timemachine_dot_ssh
+sudo rm -rf .ssh .bash_history .python_history .viminfo .wget-hsts .gnupg .lesshst .ipython .local .gitconfig .xsession*
+sudo rm -rf .factory_env
+sudo cp -r $current_env .factory_env
+sudo cp $HOME/test_sound.ogg .
 
 
-echo "Removing wpa_supplicant"
-system "sudo mv $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf $HOME/wpa_supplicant.conf"
-system "sudo rm $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf*"
+#echo "Removing wpa_supplicant"
+#sudo mv $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf $HOME/wpa_supplicant.conf
+#sudo rm $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf*
+wpa_supplicant_path=$HOME/wpa_supplicant.conf 
+if [ -f $wpa_supplicant_path ]; then
+    sudo cp $wpa_supplicant_path $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf 
+fi
 
 media_dirs="/dev/sdb1 /dev/sdb2"
 
 for dir in $media_dirs; do
-    system "sudo umount $dir"
+    sudo umount $dir
 done
 
-system "sudo rm $image_file"
+sudo rm $image_file
 critical_command "sudo dd if=/dev/sdb of=$image_file bs=4M status=progress"
 
-system "sudo pishrink.sh $image_file"
+sudo pishrink.sh $image_file
 echo "Replacing wpa_supplicant and knob_sense files"
-system "sudo mv $HOME/wpa_supplicant.conf $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf"
-system "sudo mv $HOME/.knob_sense $media_folder/rootfs/home/deadhead/."
-system "sudo mv $HOME/.timemachine_options.txt $media_folder/rootfs/home/deadhead/."
-system "sudo mv $HOME/timemachine_dot_ssh $media_folder/rootfs/home/deadhead/.ssh"
+sudo mv $HOME/wpa_supplicant.conf $media_folder/rootfs/etc/wpa_supplicant/wpa_supplicant.conf
+sudo mv $HOME/.knob_sense $media_folder/rootfs/home/deadhead/.
+sudo mv $HOME/.timemachine_options.txt $media_folder/rootfs/home/deadhead/.
+sudo mv $HOME/timemachine_dot_ssh $media_folder/rootfs/home/deadhead/.ssh
 
 # NOTE: to burn an image use the command (or similar):
 # sudo sh -c "pv v2_20210625.img > /dev/sdb"
