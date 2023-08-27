1b2585ed97ea23f8da47e0755bd21213727f1473
Steve Eichblatt <steve.eichblatt@gmail.com>
2023-08-27 19:23:00 -0400

    writing to blobs from the server

--- cloud_server/app.py
+++ cloud_server/app.py
@@ -1,6 +1,7 @@
 # to run this:
 # flask --app serve_api run --host 0.0.0.0
 
+import json
 import sys
 import time
 from flask import Flask
@@ -12,6 +13,8 @@ from flask import url_for
 from timemachine import Archivary
 from timemachine import config
 
+from google.cloud import storage
+
 config.load_options()
 
 config.optd = {
@@ -20,6 +23,8 @@ config.optd = {
 "PLAY_LOSSLESS": "false",
 }
 aa = Archivary.Archivary(collection_list=config.optd["COLLECTIONS"])
+storage_client = storage.Client(project='able-folio-397115')
+bucket = storage_client.bucket("spertilo-data")
 
 app = Flask(__name__)
 
@@ -29,6 +34,24 @@ def intersect(lis1, lis2):
 
 def xcept(lis1, lis2):
     return [x for x in lis1 if not x in set(lis2)]
+    
+
+def save_blob_data(t,date, collection, i_tape):
+    id = t.identifier
+    tracks = t.tracks()
+    trackdata = {'id':id, 'collection':collection, 'venue':t.venue(), 'track_url':{x.title:x.files[0]['url'] for x in tracks}}
+    trackdata_string = json.dumps(trackdata,indent=1)
+
+    if len(trackdata_string)>0:
+        trackdata_blob_name = f"tapes/{collection}/{date}/{id}/trackdata.json"
+        trackdata_blob = bucket.blob(trackdata_blob_name)
+        trackdata_blob.upload_from_string(trackdata_string)
+        if i_tape == 0:
+            trackdata_blob_name = f"tapes/{collection}/{date}/0/trackdata.json"
+            trackdata_blob = bucket.blob(trackdata_blob_name)
+            trackdata_blob.upload_from_string(trackdata_string)
+
+    return 0
 
 def get_all_tapes(date):
     global aa
@@ -44,17 +67,22 @@ def get_all_tapes(date):
     get_anything = True
     tape_collections = []
     t = []
+
     if len(collections) > 0:
         get_anything = False
-    for tape in tapes:
+    for i_tape,tape in enumerate(tapes):
         if get_anything:
             t.append(tape)
-            tape_collections.append(tape.collection[0])
+            this_collection = tape.collection[0]
+            tape_collections.append(this_collection)
+            save_blob_data(tape,date,this_collection, i_tape)
         else:
             matches = intersect(collections, tape.collection)
             if len(matches) > 0:
+                this_collection = matches[0]
                 t.append(tape)
-                tape_collections.append(matches[0])
+                tape_collections.append(this_collection)
+                save_blob_data(tape,date,this_collection, i_tape)
     if len(t) == 0:
         print(f'no tape for {collections} on {date}')
         return {'error':f'no tape for {collections} on {date}'}, []
@@ -97,8 +125,6 @@ def urls(date):
     t.get_metadata()
     trks = t.tracks()
     result = [x.files[0]['url'] for x in trks]
-    # result_dict = {x.title: x.files[0]['url'] for x in trks} 
-    # print(f"tmp is {result_dict}")  # dict is returned in order, but received out of order?
     return {'collection':collection, 'urls':result}
 
 @app.route("/tape_ids/<date>")
