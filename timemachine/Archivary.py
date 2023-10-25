bb1f24b08b869a6fb6414fbfd701a69db4e01f64
Steve Eichblatt <steve.eichblatt@gmail.com>
2023-10-24 22:36:52 -0400

    ugh

--- timemachine/Archivary.py
+++ timemachine/Archivary.py
@@ -518,9 +518,7 @@ class PhishinTapeDownloader(BaseTapeDownloader):
         try:
             self.apikey = open(os.path.join(os.getenv("HOME"), ".phishinkey"), "r").read().rstrip()
         except Exception:
-            resp = requests.get("https://storage.googleapis.com/spertilo-data/sundry/phkey?authuser=1")
-            self.apikey = resp.json()['phkey'].rstrip()
-            resp.close()
+            self.apikey = '8003bcd8c378844cfb69aad8b0981309f289e232fb417df560f7192edd295f1d49226ef6883902e59b465991d0869c77'
         logger.info("api key is {self.apikey}")
         self.parms = {"sort_attr": "date", "sort_dir": "desc", "per_page": "300"}
         self.headers = {"Accept": "application/json", "Authorization": f"Bearer {self.apikey}"}
@@ -996,9 +994,7 @@ class PhishinTape(BaseTape):
         try:
             self.apikey = open(os.path.join(os.getenv("HOME"), ".phishinkey"), "r").read().rstrip()
         except Exception:
-            resp = requests.get("https://storage.googleapis.com/spertilo-data/sundry/phkey?authuser=1")
-            self.apikey = resp.json()['phkey'].rstrip()
-            resp.close()
+            self.apikey = '8003bcd8c378844cfb69aad8b0981309f289e232fb417df560f7192edd295f1d49226ef6883902e59b465991d0869c77'
         self.parms = {"sort_attr": "date", "sort_dir": "asc", "per_page": "300"}
         self.headers = {"Accept": "application/json", "Authorization": f"Bearer {self.apikey}"}
 
